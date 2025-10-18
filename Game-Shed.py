c03d659a3939b10454273ae3b6ac6124244e3548
nnw-2 <yosoyluz99@gmail.com>
2025-10-18 23:46:04 +0100

    Adding blocked events and changing where flip is called

--- Game-Shed.py
+++ Game-Shed.py
@@ -9,6 +9,9 @@ w ,h = pygame.display.get_desktop_sizes()[0]
 win = pygame.display.set_mode((w*0.5,h*0.5),flags=pygame.SCALED|pygame.RESIZABLE) # when setting window width and height
 # use sizes lower than the users resolution or resizing bugs out (can't size window smaller than sizes input in set mode)
 
+pygame.event.set_blocked(None)
+pygame.event.set_allowed([QUIT])
+
 while True:
     if pygame.event.peek(QUIT):
         pygame.quit()
@@ -16,4 +19,6 @@ while True:
     if pygame.key.get_just_released()[K_F11]:
         pygame.display.toggle_fullscreen()
         pygame.event.clear()
-    pygame.display.flip()
+        pygame.display.flip()
+
+# even if keyup and keydown events are blocked pygame.key.get_just_released()[...] will still work ig i'll keep the events blocked
