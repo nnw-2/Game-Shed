b8adea1f5b961dfa22ad149932693ebb4bdb63ce
nnw-2 <yosoyluz99@gmail.com>
2026-01-15 03:06:21 +0000

    fixing the initial scaling and changing the quit functionality to be part of the main event handling

--- Game Shed/Game-Shed.py
+++ Game Shed/Game-Shed.py
@@ -7,21 +7,29 @@ pygame.init()
 
 #pygame.system.get_pref_path("nnw-2","Game Shed") remember this for storage of settings etc
 
-pygame.event.set_blocked(None)
-pygame.event.set_allowed([QUIT,WINDOWSIZECHANGED])
+EVENTS_LIST = [QUIT,WINDOWSIZECHANGED]
 
-# line = pygame.surface.Surface((10*x_scaler,1080*y_scale))
+pygame.event.set_blocked(None)
+pygame.event.set_allowed(EVENTS_LIST)
+
+# line = pygame.surface.Surface((10*self.x_scaler,1000*self.y_scaler))
 # line.fill((255,255,255))
-# win.blit(line,(300*x_scaler,0))
+# self.win.blit(line,(300*self.x_scaler,0*self.y_scaler))
 # pygame.display.flip()
 
 class Game_Shed():
     def __init__(self):
         self.w , self.h = pygame.display.get_desktop_sizes()[0]
-        self.win = pygame.display.set_mode((self.w*0.5,self.h*0.5),flags=pygame.SCALED|pygame.RESIZABLE)
+        self.w *= 0.5
+        self.h *= 0.5
+        self.win = pygame.display.set_mode((self.w,self.h),flags=pygame.RESIZABLE)
         self.x_scaler = self.w/1920
         self.y_scaler = self.h/1080
         self.line_dests = ((),())
+
+    def quit_func(self,event):
+        pygame.quit()
+        exit()
 
     def size_change_func(self,event):
         self.w , self.h = event.dict["x"] , event.dict["y"]
@@ -29,6 +37,7 @@ class Game_Shed():
         self.y_scaler = self.h/1080
 
     event_funcs = {
+        QUIT : quit_func,
         WINDOWSIZECHANGED : size_change_func
     }
 
@@ -39,10 +48,7 @@ class Game_Shed():
 
     def main(self):
         while True:
-            if pygame.event.peek(QUIT):
-                pygame.quit()
-                exit()
-            for event in pygame.event.get(WINDOWSIZECHANGED):
+            for event in pygame.event.get(EVENTS_LIST):
                 self.event_funcs[event.type](self,event)
 
             if pygame.key.get_just_released()[K_F11]:
