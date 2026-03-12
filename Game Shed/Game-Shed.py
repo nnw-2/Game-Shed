1ca909dbee32f5842eefccb7c6094b53c8e82cd4
nnw-2 <yosoyluz99@gmail.com>
2026-03-12 01:29:56 +0000

    Changing to use pygame.Window instead of display

--- Game Shed/Game-Shed.py
+++ Game Shed/Game-Shed.py
@@ -2,41 +2,46 @@ import pygame
 from pygame.constants import QUIT,K_F11,VIDEORESIZE
 from sys import exit
 from UI import UI,Lines
+import os
 
 pygame.init()
 
 #pygame.system.get_pref_path("nnw-2","Game Shed") remember this for storage of settings etc
 
 EVENTS_LIST = [QUIT,VIDEORESIZE]
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 pygame.event.set_blocked(None)
 pygame.event.set_allowed(EVENTS_LIST)
 
-# line = pygame.surface.Surface((10*self.x_scaler,1000*self.y_scaler))
-# line.fill((255,255,255))
-# self.win.blit(line,(300*self.x_scaler,0*self.y_scaler))
-# pygame.display.flip()
 
 class Game_Shed():
     def __init__(self):
         self.w , self.h = pygame.display.get_desktop_sizes()[0]
-        self.w *= 0.5
-        self.h *= 0.5
-        self.win = pygame.display.set_mode((self.w,self.h),flags=pygame.RESIZABLE)
+        self.w = int(self.w * 0.5)
+        self.h = int(self.h * 0.5)
+
+        self.win_actual = pygame.Window("Game Shed",(self.w,self.h),resizable=True)
+        self.win_actual.set_icon(pygame.image.load(BASE_PATH + "/Images/icon.png"))
+        self.win = self.win_actual.get_surface()
+        self.is_fullscreen = False
         self.x_scaler = self.w/1920
         self.y_scaler = self.h/1080
         UI.x_scale = self.x_scaler
         UI.y_scale = self.y_scaler
         self.Lines1 = pygame.sprite.Group()
 
-        Lines((255,255,255),(960,540),(0,0),self.Lines1)
+        Lines((255,255,255),(1920,10),(0,120),self.Lines1)
+        Lines((255,255,255),(10,1080),(100,0),self.Lines1)
 
     def quit_func(self,event):
+        self.win_actual.destroy()
         pygame.quit()
         exit()
 
     def win_size_change_func(self,event):
         self.w , self.h = event.w , event.h
+        self.win = self.win_actual.get_surface()
         self.x_scaler = self.w/1920
         self.y_scaler = self.h/1080
         UI.x_scale = self.x_scaler
@@ -48,10 +53,8 @@ class Game_Shed():
         
     def render(self):
         self.win.fill((0,0,0)) # want to change to fill based on a background var in future or have the win be an image
-
         self.Lines1.draw(self.win)
-
-        pygame.display.flip()
+        self.win_actual.flip()
 
     event_funcs = {
         QUIT : quit_func,
@@ -66,10 +69,17 @@ class Game_Shed():
                 self.event_funcs[event.type](self,event)
 
             if pygame.key.get_just_released()[K_F11]:
-                pygame.display.toggle_fullscreen()
+                self.is_fullscreen = not self.is_fullscreen
+                
+                if self.is_fullscreen:
+                    self.win_actual.set_fullscreen(True)
+                else:
+                    self.win_actual.set_windowed()
+                    
                 pygame.event.clear()
 
-                self.w, self.h = self.win.get_size()
+                self.w, self.h = self.win_actual.size
+                self.win = self.win_actual.get_surface()
                 
                 self.x_scaler = self.w / 1920
                 self.y_scaler = self.h / 1080
@@ -80,7 +90,3 @@ class Game_Shed():
                 self.render()
 
 Game_Shed().main()
-
-
-
-# I want to move to using pygame.Window instead of display setmode
