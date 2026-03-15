4f79d0cf34aa9cd095e3d706758aa971b85baf4d
nnw-2 <yosoyluz99@gmail.com>
2026-03-15 00:03:44 +0000

    Adding images as part of UI

--- Game Shed/Game-Shed.py
+++ Game Shed/Game-Shed.py
@@ -1,7 +1,7 @@
 import pygame
-from pygame.constants import QUIT,K_F11,VIDEORESIZE
+from pygame.constants import QUIT,K_F11,VIDEORESIZE , K_a
 from sys import exit
-from UI import UI,Lines
+from UI import UI,Lines,Images,Colour_Changing_Imgaes
 import os
 
 pygame.init()
@@ -30,9 +30,15 @@ class Game_Shed():
         UI.x_scale = self.x_scaler
         UI.y_scale = self.y_scaler
         self.Lines1 = pygame.sprite.Group()
+        self.Images1 = pygame.sprite.Group()
+        self.Const_Colour_Imgs = pygame.sprite.Group()
+        self.Colour_Changing_Imgs = pygame.sprite.Group()
 
         Lines((255,255,255),(1920,10),(0,120),self.Lines1)
         Lines((255,255,255),(10,1080),(100,0),self.Lines1)
+        Colour_Changing_Imgaes(BASE_PATH + "/Images/icon.png",(255,255,255),(100,100),(500,500),self.Images1,self.Colour_Changing_Imgs) # this one is just for testing
+        Images(BASE_PATH + "/Images/icon.png",(100,100),(700,700),self.Images1,self.Const_Colour_Imgs)
+
 
     def quit_func(self,event):
         self.win_actual.destroy()
@@ -48,12 +54,14 @@ class Game_Shed():
         UI.y_scale = self.y_scaler
 
         self.Lines1.update(window_changed_size=True)
+        self.Images1.update(window_changed_size=True)
 
         self.render()
         
     def render(self):
         self.win.fill((0,0,0)) # want to change to fill based on a background var in future or have the win be an image
         self.Lines1.draw(self.win)
+        self.Images1.draw(self.win)
         self.win_actual.flip()
 
     event_funcs = {
@@ -87,6 +95,11 @@ class Game_Shed():
                 UI.y_scale = self.y_scaler
                 
                 self.Lines1.update(window_changed_size=True)
+                self.Images1.update(window_changed_size=True)
+                self.render()
+            if pygame.key.get_just_released()[K_a]:
+                self.Colour_Changing_Imgs.update(colour_change=(0,0,255))
+                self.Lines1.update(colour_change=(255,0,0))
                 self.render()
 
 Game_Shed().main()
--- Game Shed/UI.py
+++ Game Shed/UI.py
@@ -19,10 +19,15 @@ class UI(pygame.sprite.Sprite):
     def change_size(self):
         ...
 
+    def change_colour(self,colour):
+        ...
+
     def update(self, **kwargs):
         if kwargs.get("window_changed_size", False):
             self.change_dest()
             self.change_size()
+        if kwargs.get("colour_change", False):
+            self.change_colour(kwargs.get("colour_change"))
 
 class Lines(UI):
     def __init__(self,colour:tuple[int,int,int], initial_size:tuple[int,int], destination:tuple[int,int], *sprite_groups):
@@ -41,4 +46,34 @@ class Lines(UI):
         self.colour = colour
         self.image.fill(self.colour)
 
-    
+class Images(UI):
+    def __init__(self,image:str, initial_size:tuple[int,int], destination:tuple[int,int], *sprite_groups):
+        super().__init__(initial_size,destination,*sprite_groups)
+        self.initial_image = pygame.image.load(image).convert_alpha()
+
+        self.change_size()
+        self.change_dest()
+
+    def change_size(self):
+        self.image = pygame.transform.smoothscale(self.initial_image, (int(self.initial_size[0] * UI.x_scale),
+                                                                       int(self.initial_size[1] * UI.y_scale))).convert_alpha()
+        self.rect.size = self.image.get_size()
+
+class Colour_Changing_Imgaes(Images):
+    def __init__(self, image: str, colour:tuple[int,int,int], initial_size: tuple[int, int], destination: tuple[int, int], *sprite_groups):
+        self.colour = colour
+        super().__init__(image, initial_size, destination, *sprite_groups)
+        self.image:pygame.Surface = pygame.Surface((1,1),pygame.SRCALPHA)
+
+        self.change_size()
+        self.change_dest()
+
+    def change_size(self):
+        super().change_size()
+        self.change_colour(self.colour)
+
+    def change_colour(self,colour):
+        self.colour = colour
+        scaled_white_image = pygame.transform.smoothscale(self.initial_image, self.image.get_size())
+        self.image.fill((colour[0], colour[1], colour[2], 255))
+        self.image.blit(scaled_white_image, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
