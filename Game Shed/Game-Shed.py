ce5fa1dcd11014a0c063825911273eae8ec0ddfd
nnw-2 <yosoyluz99@gmail.com>
2026-03-19 01:41:30 +0000

    implemented settings file

--- Game Shed/Game-Shed.py
+++ Game Shed/Game-Shed.py
@@ -7,8 +7,6 @@ import json
 
 pygame.init()
 
-#pygame.system.get_pref_path("nnw-2","Game Shed") remember this for storage of settings etc
-
 EVENTS_LIST = [QUIT,VIDEORESIZE,WINDOWFOCUSLOST,WINDOWFOCUSGAINED]
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,12 +16,20 @@ pygame.event.set_allowed(EVENTS_LIST)
 
 class Game_Shed():
     def __init__(self):
+        self.pref_path = pygame.system.get_pref_path("nnw-2","Game Shed")
+        self.settings_path = os.path.join(self.pref_path,"settings.json")
+        self.settings = self.load_settings()
+
+        self.line_colour = self.settings["line_colour"]
+        self.icon_colour = self.settings["icon_colour"]
+        self.background_colour = self.settings["background_colour"]
+
         self.w , self.h = pygame.display.get_desktop_sizes()[0]
         self.w = int(self.w * 0.5)
         self.h = int(self.h * 0.5)
 
         self.win_actual = pygame.Window("Game Shed",(self.w,self.h),resizable=True)
-        self.win_actual.set_icon(pygame.image.load(BASE_PATH + "/Images/icon.png"))
+        self.win_actual.set_icon(pygame.image.load(os.path.join(BASE_PATH,"Images","icon.png")))
         self.win = self.win_actual.get_surface()
         self.clock = pygame.Clock()
         self.fps = 30
@@ -37,15 +43,30 @@ class Game_Shed():
         self.Const_Colour_Imgs = pygame.sprite.Group()
         self.Colour_Changing_Imgs = pygame.sprite.Group()
 
-        Lines((255,255,255),(1920,10),(0,120),self.Lines1)
-        Lines((255,255,255),(10,1080),(100,0),self.Lines1)
-        Colour_Changing_Images(BASE_PATH + "/Images/icon.png",(0,0,255),(100,100),(500,500),self.Images1,self.Colour_Changing_Imgs) # this one is just for testing
-        Images(BASE_PATH + "/Images/icon.png",(100,100),(700,700),self.Images1,self.Const_Colour_Imgs)
+        Lines(self.line_colour,(1920,10),(0,120),self.Lines1)
+        Lines(self.line_colour,(10,1080),(100,0),self.Lines1)
+        Colour_Changing_Images(os.path.join(BASE_PATH,"Images","icon.png"),self.icon_colour,(100,100),(500,500),self.Images1,self.Colour_Changing_Imgs) # this one is just for testing
+        Images(os.path.join(BASE_PATH,"Images","icon.png"),(100,100),(700,700),self.Images1,self.Const_Colour_Imgs)
 
+    def load_settings(self):
+        if os.path.exists(self.settings_path):
+            with open(self.settings_path, "r") as settings_f:
+                return json.load(settings_f)
+
+        return {
+            "line_colour" : (255,255,255),
+            "icon_colour" : (255,255,255),
+            "background_colour" : (0,0,0)
+        }
+
+    def save_settings(self):
+        with open(self.settings_path, "w") as settings_f:
+            json.dump(self.settings, settings_f, indent=4)
 
     def quit_func(self,event):
         self.win_actual.destroy()
         pygame.quit()
+        self.save_settings()
         exit()
 
     def win_size_change_func(self,event):
@@ -68,7 +89,9 @@ class Game_Shed():
         self.fps = 30
 
     def render(self):
-        self.win.fill((0,0,0)) # want to change to fill based on a background var in future or have the win be an image
+        self.win.fill(self.background_colour) 
+        #in future when I allow an image for a background keep the win.fill(self.background_colour) so the
+        #background light can be customised if they want the opacity of the image to be lower
         self.Lines1.draw(self.win)
         self.Images1.draw(self.win)
         self.win_actual.flip()
